b5248c8acf654dd4a9093e12f18c509cc96188f0
Your Name <your-email@gmail.com>
2026-03-08 11:38:56 +0600

    Dinamic Login Page

---
+++ script.js
@@ -0,0 +1,15 @@
+const loginBtn = document.getElementById('loginBtn');
+
+loginBtn.addEventListener('click', function () {
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  if (username === 'admin' && password === 'admin123') {
+    alert('Login Successful');
+
+    // main page এ redirect
+    window.location.href = 'main.html';
+  } else {
+    alert('Wrong Username or Password');
+  }
+});
